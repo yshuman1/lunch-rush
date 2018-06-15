48fcd0133409938211151ecc1860c30af1dab4d6
Yasin Shuman <email@yasin.io>
2018-06-15 00:16:36 -0700

    saving progress while i debug

--- src/Application.js
+++ src/Application.js
@@ -1,22 +1,37 @@
-import React, { Component } from 'react';
-import { auth, database } from './firebase';
-import CurrentUser from './CurrentUser';
-import SignIn from './SignIn';
-import NewRestaurant from './NewRestaurant';
-import Restaurants from './Restaurants';
-import './Application.css';
+import React, { Component } from "react";
+import { auth, database } from "./firebase";
+import CurrentUser from "./CurrentUser";
+import SignIn from "./SignIn";
+import NewRestaurant from "./NewRestaurant";
+import Restaurants from "./Restaurants";
+import "./Application.css";
 
 class Application extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      currentUser: null
+    };
+  }
+  componentDidMount() {
+    auth.onAuthStateChanged(currentUser => {
+      console.log("Auth Change", currentUser);
+      this.setState(currentUser);
+    });
   }
 
   render() {
+    const { currentUser } = this.state;
+
     return (
       <div className="Application">
         <header className="Application--header">
           <h1>Lunch Rush</h1>
         </header>
+        <div>
+          {!currentUser && <SignIn />}
+          {currentUser && <CurrentUser user={currentUser} />}
+        </div>
       </div>
     );
   }
